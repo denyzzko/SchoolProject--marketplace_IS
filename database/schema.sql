e6a59fe038b966242d933e94bff54701c6ae3fd9
denyzzko <denis.milistenfer1@gmail.com>
2024-11-25 10:19:16 +0100

    fix database scheme

--- database/schema.sql
+++ database/schema.sql
@@ -22,10 +22,7 @@ CREATE TABLE Category (
     parent_category INT,
     name VARCHAR(100) NOT NULL,
     image_path VARCHAR(255),
-    CONSTRAINT fk_category_parent
-        FOREIGN KEY (parent_category)
-        REFERENCES Category(category_id)
-        ON DELETE CASCADE
+    FOREIGN KEY (parent_category) REFERENCES Category(category_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Offer (
@@ -53,12 +50,12 @@ CREATE TABLE Attribute (
 CREATE TABLE Ordr (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
-    offer_id INT NOT NULL,
+    offer_id INT NULL,
     quantity DECIMAL(6,3),
     date DATE NOT NULL,
     status ENUM('pending', 'confirmed', 'rejected') NOT NULL,
     FOREIGN KEY (user_id) REFERENCES Usr(user_id) ON DELETE CASCADE,
-    FOREIGN KEY (offer_id) REFERENCES Offer(offer_id) ON DELETE CASCADE
+    FOREIGN KEY (offer_id) REFERENCES Offer(offer_id) ON DELETE SET NULL
 );
 
 CREATE TABLE Review (
